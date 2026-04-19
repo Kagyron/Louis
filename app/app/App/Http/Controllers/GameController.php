39e0be6ed145334b99370367f242a30bb9d892dc
Louis Kreuzberg <louiskreuzberg4@gmail.com>
2026-04-19 13:19:26 +0000

    no cheating

--- app/app/App/Http/Controllers/GameController.php
+++ app/app/App/Http/Controllers/GameController.php
@@ -175,7 +175,7 @@ class GameController extends Controller
         // Once all the checks have passed, you can finally update the game board by calling
         // $game->setSpace( $x, $y, GameMark::Circle ).
         // [ The code to check if the space is free goes here ]
-        if ($game->getSpace($x, $y) === null) {
+        if ($game->getSpace($x, $y) === Empty) {
     $game->setSpace($x, $y, GameMark::Circle);
 }
 
@@ -183,8 +183,7 @@ class GameController extends Controller
         //return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
 
         // [ The code to update the game board goes here ]
-        
-           if ($game->getSpace($x, $y) !==null) {
+           if ($game->getSpace($x, $y) !== GameMark::Empty) {
             return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
            }
         // Saving the game board and output it to the player
